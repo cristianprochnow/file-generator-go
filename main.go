c09220996c213d0b7afd85ab6f6c8b5759087a0f
Cristian Prochnow <cristianprochnow@gmail.com>
2024-01-31 22:49:56 -0300

    feat: finish first full script

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -22,7 +25,93 @@ func main() {
 	readFileName(&fileNameFlag)
 	space()
 
-	fmt.Println(textForFiles, filesAmount, checkOutputFlag, fileNameFlag)
+	buildFiles(fileNameFlag, textForFiles, filesAmount)
+	printContent(textForFiles, checkOutputFlag)
+}
+
+func getFilePath() string {
+	return "./files/"
+}
+
+func printContent(content string, outputFlag string) {
+	if outputFlag == "s" {
+		space()
+		fmt.Println("Conteúdo inserido: ", content)
+		space()
+	}
+}
+
+func getFileName(chosedFileOption string, writtenText string) string {
+	var fileName string
+
+	switch chosedFileOption {
+	case "p":
+		var words []string
+
+		for index, word := range writtenText {
+			words[index] = string(word)
+
+			if word == ' ' {
+				break
+			}
+		}
+
+		fileName = strings.Join(words, "")
+		break
+
+	case "l":
+		fileName = string(writtenText[0])
+		break
+
+	case "d":
+	default:
+		fileName = time.Now().Format("2006-01-02-15-04-05")
+		break
+	}
+
+	return fileName
+}
+
+func buildFiles(chosedFileOption string, writtenText string, filesAmount int) {
+	filePath := getFilePath() + getFileName(chosedFileOption, writtenText)
+	byteText := []byte(writtenText)
+
+	var index int
+	for index = 0; index < filesAmount; index++ {
+		fullFileName := filePath + "_(" + strconv.Itoa(index) + ").txt"
+
+		if !fileExists(fullFileName) {
+			initFile(fullFileName)
+		}
+
+		writeFile(fullFileName, byteText)
+	}
+}
+
+func writeFile(filePath string, content []byte) {
+	message := os.WriteFile(filePath, content, 0644)
+
+	check(message)
+}
+
+func initFile(fullFileName string) {
+	fileObject, message := os.Create(fullFileName)
+
+	check(message)
+
+	defer fileObject.Close()
+}
+
+func check(message error) {
+	if message != nil {
+		panic(message)
+	}
+}
+
+func fileExists(filePath string) bool {
+	_, errorMessage := os.Stat(filePath)
+
+	return !errors.Is(errorMessage, os.ErrNotExist)
 }
 
 func checkToShowOutput(checkOutput *string) {
@@ -42,7 +131,7 @@ func readFilesAmount(filesAmount *int) {
 
 func readFileName(fileNameFlag *string) {
 	fmt.Println("Qual das opções deseja em relação ao nome do arquivo:")
-	fmt.Println("[d] - Data atual")
+	fmt.Println("[d] - Data atual (Padrão)")
 	fmt.Println("[p] - Primeira palavra do texto")
 	fmt.Println("[l] - Primeira letra do texto")
 
